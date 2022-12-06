138a6297c9f390c5d59aab100fb6220a25fecd13
Juan Jose Guapo <119938909+JuanjDes@users.noreply.github.com>
2022-12-06 18:08:19 +0100

    Add files via upload
    Crear una función que sume 3 variables. Asignarles valores en el main y mostrar resultado.

---
+++ Introduccion a programacion/Ejercicio 3/Suma/Ejercicio3a.java
@@ -0,0 +1,15 @@
+public class Ejercicio3a
+{
+    public static void main(String[] args)
+    {
+        int resultado;
+
+        resultado = ejercicio3a(10, 20, 30);
+        System.out.println(resultado);
+    }
+
+    public static int ejercicio3a(int a, int b, int c)
+    {
+        return a+b+c;
+    }
+}
