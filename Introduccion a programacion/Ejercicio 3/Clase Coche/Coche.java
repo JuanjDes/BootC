61aa5382b45d7b8e1d6662a4378bd3137cf3f15c
Juan Jose Guapo <119938909+JuanjDes@users.noreply.github.com>
2022-12-07 04:39:46 +0100

    Update Coche.java

--- Introduccion a programacion/Ejercicio 3/Clase Coche/Coche.java
+++ Introduccion a programacion/Ejercicio 3/Clase Coche/Coche.java
@@ -4,7 +4,4 @@ public class Coche
     // Se crea la función sumapuertas cuyo valor es la variable puertas incrementada en 4
     public static int sumapuertas (int puertas) {return puertas + 4;}
 
-    // También se crea la función sumapuertas sin parámetros, para cuando es llamada en el Main sin parámetros
-    public void sumapuertas() {}
 }
-
