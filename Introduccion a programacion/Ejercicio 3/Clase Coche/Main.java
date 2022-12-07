6a378f0e101cb08ae5e86d249446f78c9c6a20fa
Juan Jose Guapo <119938909+JuanjDes@users.noreply.github.com>
2022-12-07 04:40:17 +0100

    Update Main.java

--- Introduccion a programacion/Ejercicio 3/Clase Coche/Main.java
+++ Introduccion a programacion/Ejercicio 3/Clase Coche/Main.java
@@ -2,11 +2,11 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Se crea el objeto miCoche de la clase Coche
         Coche miCoche = new Coche();
-        miCoche.sumapuertas();
 
-        // Sacamos por pantalla el valor de la función sumapuertas incrementado en 1
-        System.out.println(miCoche.sumapuertas(1));
+        // Sacamos en pantalla el valor de la función sumapuertas, dentro del objeto miCoche, incrementado en 1
+        System.out.println("Número de puertas: " + miCoche.sumapuertas(1));
 
     }
 }
