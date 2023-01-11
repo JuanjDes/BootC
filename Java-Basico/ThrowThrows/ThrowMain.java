e61daed25f691f3589352168c3ebcb6498fd8a24
Juan Jose Guapo <juanjguapo@gmail.com>
2023-01-11 09:55:09 +0100

    Update ThrowMain.java

--- Java-Basico/ThrowThrows/ThrowMain.java
+++ Java-Basico/ThrowThrows/ThrowMain.java
@@ -1,3 +1,5 @@
+                                            // USO DE THROW Y THROWS PARA CONTROLAR EXCEPCIONES
+
 package gestionexcepciones;
 
 import java.util.Scanner;
@@ -6,6 +8,7 @@ public class ThrowMain {
 
     public static void main(String[] args) {
 
+        // Capturamos la excepción lanzada por el método leerNombres()
         try {
             leerNombres();
         } catch (NameFormatException e) {
@@ -15,8 +18,7 @@ public class ThrowMain {
 
 
     /** Método que lee nombres de consola y verifica que tengan longitud igual o mayor a 8 caracteres **/
-
-    private static void leerNombres() throws NameFormatException {
+    private static void leerNombres() throws NameFormatException {   // Con throws le indicamos al método que va a lanzar una excepcion
 
         Scanner teclado = new Scanner(System.in);
         System.out.println("Introduce un nombre");
@@ -25,7 +27,7 @@ public class ThrowMain {
             String nombre = teclado.nextLine();
             if (nombre.length() < 8) {
                 teclado.close();
-                throw new NameFormatException ("El nombre debe contener mínimo 8 caracteres");
+                throw new NameFormatException ("El nombre debe contener mínimo 8 caracteres");  // Con throw lanzamos la excepción
             }
         }
         teclado.close();
