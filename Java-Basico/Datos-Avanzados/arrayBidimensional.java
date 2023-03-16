6654274de79a843ce36bb99b481220b7a3ed0c9a
Juan Jose Guapo <juanjguapo@gmail.com>
2023-03-16 17:41:05 +0100

    Add files via upload

---
+++ Java-Basico/Datos-Avanzados/arrayBidimensional.java
@@ -0,0 +1,28 @@
+public class arrayBidimensional {
+
+    public static void main(String[] args) {
+
+        // Vamos a crear un array bidimensional con 2 filas y 4 columnas
+        int arrayBidi[][] = new int[2][4];
+        arrayBidi[0][0] = 1; // Fila 0 columna 0 contiene el valor 1
+        arrayBidi[0][1] = 2; // Fila 0 columna 1 contiene el valor 1
+        arrayBidi[0][2] = 3; // Fila 0 columna 2 contiene el valor 1
+        arrayBidi[0][3] = 4; // Fila 0 columna 3 contiene el valor 1
+
+        arrayBidi[1][0] = 10; // Fila 1 columna 0 contiene el valor 10
+        arrayBidi[1][1] = 20; // Fila 1 columna 1 contiene el valor 20
+        arrayBidi[1][2] = 30; // Fila 1 columna 2 contiene el valor 30
+        arrayBidi[1][3] = 40; // Fila 1 columna 3 contiene el valor 40
+
+        // Ahora recorremos el array bidimensional. Utilizando un for dentro de otro for
+        for (int i = 0; i < arrayBidi.length; i++) {
+            System.out.println("Valor de i: " + i);
+            // Con este segundo for recorremos las columnas de la fila i
+            for (int j = 0; j < arrayBidi[1].length; j++) {
+                System.out.print("Estoy en Fila: " + i + ", Columna: " + j);
+                System.out.println(" - Valor: " + arrayBidi[i][j]);
+            }
+        }
+
+    }
+}
---
+++ Java-Basico/Datos-Avanzados/datoString.java
@@ -0,0 +1,53 @@
+
+public class datoString {
+
+    public static void main(String[] args) {
+        // Declaración de variables
+        String cadena = "MenSAJE de Txto";
+        boolean inicio = cadena.startsWith("Men");
+        boolean fin = cadena.endsWith("Texto");
+        int cadenaLen = cadena.length();
+
+        System.out.println("La longitud de la cadena de texto es " + cadenaLen + " caracteres");
+
+        // Método toLowerCase() para convertir cadena a minúsculas
+        String cadenaMinus = cadena.toLowerCase();
+        System.out.println("Convertimos a minúsculas: " + cadenaMinus);
+
+        // Método toUpperCase() para convertir cadena a mayúsculas
+        String cadenaMayus = cadena.toUpperCase();
+        System.out.println("Convertimos a mayúsculas: " + cadenaMayus);
+
+        System.out.print("¿ La cadena empieza por Men ? ");
+        if (inicio) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+
+        System.out.print("¿ La cadena acaba por Texto ? ");
+        if (fin) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
+
+        // Guardamos en letra con el método charAt(), el valor de la posición 3 de cadena
+        char letra = cadena.charAt(3);
+        System.out.println("La posición 3 de cadena contiene: " + letra);
+
+        // Recorremos el String cadena y vamos mostrando el valor de cada posición
+        for (int i = 0; i < cadena.length(); i++) {
+            System.out.println("Caracter actual: " + cadena.charAt(i));
+        }
+
+        /* Mostramos el String cadena al revés. Para ello i se posiciona en la última posición
+            del String, mostramos el valor de la posición y restamos 1 a i. Y volvemos a mostrar
+            la posición. Haste que i llegue a la posición 0 */
+        for (int i = cadenaLen - 1; i >=0; i--) {
+            char letrab = cadena.charAt(i);
+            System.out.print(letrab);
+        }
+
+    }
+}
